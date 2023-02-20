2333cb073ecc4d3d02c57866da085a1790164239
Sayam1007 <sayamsobti2gmail.com>
2023-02-20 11:51:12 +0530

    First update

--- gitpush.sh
+++ gitpush.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
+
 echo "Enter github email id"
 read email
 echo "Enter github username"
 read username
+
 git config --global user.email "$email"
 git config --global user.name "$username"
 
-echo "Enter the local repo path"
+echo "Enter the folder path"
 read path
 cd $path
 
 git init
 echo "Adding all the files to staging area"
+
 git add .
 echo "Enter any commit message"
 read msg
 git commit -m "$msg"
+echo "enter the remote url "
+read url
+git remote add origin  "$url"
+
+git push -u origin master
+
+
 
 
